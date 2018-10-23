790a0808348f364085c1274a10cde4a22b8a9b22
Andrew Fecheyr <andrew@bedesign.be>
2018-10-23 09:26:50 +0200

    /shuffle /random and /dice commands

--- lib/shuffle_router.ex
+++ lib/shuffle_router.ex
@@ -2,14 +2,44 @@ defmodule ShuffleRouter do
   use Plug.Router
 
   plug Plug.Logger
+  plug Plug.Parsers, parsers: [:urlencoded]
   plug :match
   plug :dispatch
 
-  get "/hello" do
-    send_resp(conn, 200, "world")
+  post "/shuffle" do
+    shuffled = conn.params["text"]
+    |> String.split(" ")
+    |> Enum.shuffle
+
+    slack_respond(conn, shuffled)
+  end
+
+  post "/random" do
+    item = conn.params["text"]
+    |> String.split(" ")
+    |> Enum.random
+
+    slack_respond(conn, item)
+  end
+
+  post "/dice" do
+    roll = :rand.uniform(6)
+
+    slack_respond(conn, roll)
   end
 
   match _ do
     send_resp(conn, 404, "oops")
+  end
+
+  defp slack_respond(conn, text) do
+    json = Jason.encode!(%{
+      response_type: "in_channel",
+      text: text,
+    })
+
+    conn
+    |> put_resp_content_type("application/json")
+    |> send_resp(200, json)
   end
 end
--- mix.exs
+++ mix.exs
@@ -22,7 +22,8 @@ defmodule Shuffler.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:jason, "~> 1.1"},
     ]
   end
 end
