42789a12d8421184d63b5408069df3a8dda8764a
Andrew Fecheyr <andrew@bedesign.be>
2018-11-03 21:59:03 +0000

    Improve PRNG seed
    From https://hashrocket.com/blog/posts/the-adventures-of-generating-random-numbers-in-erlang-and-elixir#using-code-rand-code-in-elixir

--- lib/shuffler/application.ex
+++ lib/shuffler/application.ex
@@ -6,6 +6,8 @@ defmodule Shuffler.Application do
   use Application
 
   def start(_type, _args) do
+    better_rand_seed()
+
     # List all child processes to be supervised
     children = [
       Plug.Cowboy.child_spec(scheme: :http, plug: ShuffleRouter, options: [port: 4000])
@@ -15,5 +17,12 @@ defmodule Shuffler.Application do
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Shuffler.Supervisor]
     Supervisor.start_link(children, opts)
+  end
+
+  defp better_rand_seed do
+    IO.puts "Providing the PRNG with a better seed and algo"
+    # TODO try and get some bytes from random.org
+    << i1 :: unsigned-integer-32, i2 :: unsigned-integer-32, i3 :: unsigned-integer-32>> = :crypto.strong_rand_bytes(12)
+    :rand.seed(:exsplus, {i1, i2, i3})
   end
 end
